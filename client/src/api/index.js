29cb270bb4a41a8f509f2f19ed1d61511d73096f
Omsindal01 <omprakashsindal01@gmail.com>
2023-12-01 14:05:48 +0530

    added a gitignore file

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseUrl: "https://socila-media-app-mern-stack.herokuapp.com",
+  baseUrl: "http://localhost:5000/",
 });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile"))
